40ce257839d6a8d6a3b234c63680097ec690cea9
chkinglee <627021415@qq.com>
2020-04-25 08:01:53 +0800

    init

---
+++ src/main/java/org/chkinglee/norn/cim/CimApplication.java
@@ -0,0 +1,13 @@
+package org.chkinglee.norn.cim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CimApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CimApplication.class, args);
+    }
+
+}
